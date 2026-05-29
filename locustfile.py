3728f2b882fc59b2071ad09532b6ba880cc60efb
sshivam48045-cloud <sshivam48045@gmail.com>
2026-05-29 21:36:48 +0530

    Update locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -5,10 +5,10 @@ class APIUser(HttpUser):
 
     @task(1)
     def test_baseline_sync(self):
-        # Ee fail hoga under load
+        
         self.client.get("http://localhost:8001/api/v1/crawl", name="GET /baseline")
 
     @task(3)
     def test_decoupled_async(self):
-        # Ee rocket liyan udega!
+        
         self.client.post("http://localhost:8000/api/v1/crawl_async", name="POST /decoupled")
